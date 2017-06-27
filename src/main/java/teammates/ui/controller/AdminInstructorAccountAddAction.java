fe735222adc21aa29d81ba1ef387dcb9a7b9e3d8
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2017-06-27 16:32:41 +0800

    Finish extracting common add instructor logic to AdminAddInstructorLogic

--- src/main/java/teammates/ui/controller/AdminInstructorAccountAddAction.java
+++ src/main/java/teammates/ui/controller/AdminInstructorAccountAddAction.java
@@ -2,33 +2,20 @@ package teammates.ui.controller;
 
 import java.util.List;
 
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-
-import teammates.common.datatransfer.DataBundle;
-import teammates.common.datatransfer.attributes.FeedbackResponseCommentAttributes;
 import teammates.common.datatransfer.attributes.InstructorAttributes;
-import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.exception.EmailSendingException;
-import teammates.common.exception.EntityDoesNotExistException;
 import teammates.common.exception.InvalidParametersException;
 import teammates.common.exception.TeammatesException;
 import teammates.common.util.Config;
 import teammates.common.util.Const;
 import teammates.common.util.EmailWrapper;
-import teammates.common.util.FieldValidator;
-import teammates.common.util.JsonUtils;
 import teammates.common.util.Logger;
 import teammates.common.util.SanitizationHelper;
 import teammates.common.util.StatusMessage;
 import teammates.common.util.StatusMessageColor;
 import teammates.common.util.StringHelper;
-import teammates.common.util.Templates;
 import teammates.common.util.Url;
 import teammates.logic.api.EmailGenerator;
-import teammates.logic.backdoor.BackDoorLogic;
 import teammates.logic.core.AdminAddInstructorLogic;
 import teammates.ui.pagedata.AdminHomePageData;
 
@@ -86,7 +73,7 @@ public class AdminInstructorAccountAddAction extends Action {
         String courseId;
 
         try {
-            courseId = importDemoData(data);
+            courseId = adminAddInstructorLogic.createDemoDataForCourseInstructor(data);
         } catch (Exception e) {
             return createInstructorImportRetryAjaxResult(data, e);
         }
@@ -145,7 +132,7 @@ public class AdminInstructorAccountAddAction extends Action {
     }
 
     /**
-     * Creates an AJAX result with a link to retry import data failure
+     * Creates an AJAX result with a link to retry import data failure.
      */
     private ActionResult createInstructorImportRetryAjaxResult(AdminHomePageData data, Exception e) {
         String retryUrl = Const.ActionURIs.ADMIN_INSTRUCTORACCOUNT_ADD;
@@ -231,150 +218,5 @@ public class AdminInstructorAccountAddAction extends Action {
                                                                Const.LENGTH_FOR_NAME_EMAIL_INSTITUTION));
         }
         return result;
-    }
-
-    /**
-     * Imports Demo course to new instructor.
-     * @param pageData data from AdminHomePageData
-     * @return the ID of Demo course
-     */
-    private String importDemoData(AdminHomePageData pageData)
-            throws InvalidParametersException, EntityDoesNotExistException {
-        String courseId = generateDemoCourseId(pageData.instructorEmail);
-
-        DataBundle demoDataBundle = createDemoDataBundle(courseId, pageData);
-
-        persistDataBundle(demoDataBundle);
-
-        createAndPutDocuments(pageData, courseId);
-
-        return courseId;
-    }
-
-    /**
-     * Create and put documents for the demo data into the Index for use in App Engine's Search API
-     */
-    private void createAndPutDocuments(AdminHomePageData pageData, String courseId) {
-        List<FeedbackResponseCommentAttributes> frComments =
-                logic.getFeedbackResponseCommentForGiver(courseId, pageData.instructorEmail);
-        List<StudentAttributes> students = logic.getStudentsForCourse(courseId);
-        List<InstructorAttributes> instructors = logic.getInstructorsForCourse(courseId);
-
-        logic.putFeedbackResponseCommentDocuments(frComments);
-        logic.putStudentDocuments(students);
-        logic.putInstructorDocuments(instructors);
-    }
-
-    private void persistDataBundle(DataBundle dataBundle) throws InvalidParametersException, EntityDoesNotExistException {
-        // although BackDoorLogic is not for production use, it is more convenient to use it here, or should we not use
-        // it?
-        BackDoorLogic backDoorLogic = new BackDoorLogic();
-        backDoorLogic.persistDataBundle(dataBundle);
-    }
-
-    private DataBundle createDemoDataBundle(String courseId, AdminHomePageData pageData) {
-        DateTime demoFeedbackSessionEndDateTime = new DateTime(DateTimeZone.UTC)
-                .withHourOfDay(23).withMinuteOfHour(59).plusYears(1);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd hh:mm a z");
-
-        String demoDataBundleJsonString = Templates.populateTemplate(Templates.INSTRUCTOR_SAMPLE_DATA,
-                // replace email
-                "teammates.demo.instructor@demo.course", pageData.instructorEmail,
-                // replace name
-                "Demo_Instructor", pageData.instructorName,
-                // replace course
-                "demo.course", courseId,
-                // update feedback session time
-                "2013-04-01 11:59 PM UTC", demoFeedbackSessionEndDateTime.toString(dateTimeFormatter));
-
-        return JsonUtils.fromJson(demoDataBundleJsonString, DataBundle.class);
-    }
-
-    // Strategy to Generate New Demo Course Id:
-    // a. keep the part of email before "@"
-    //    replace "@" with "."
-    //    replace email host with their first 3 chars. eg, gmail.com -> gma
-    //    append "-demo"
-    //    to sum up: lebron@gmail.com -> lebron.gma-demo
-    //
-    // b. if the generated courseId already exists, create another one by appending a integer to the previous courseId.
-    //    if the newly generate id still exists, increment the id, until we find a feasible one
-    //    eg.
-    //    lebron@gmail.com -> lebron.gma-demo  // already exists!
-    //    lebron@gmail.com -> lebron.gma-demo0 // already exists!
-    //    lebron@gmail.com -> lebron.gma-demo1 // already exists!
-    //    ...
-    //    lebron@gmail.com -> lebron.gma-demo99 // already exists!
-    //    lebron@gmail.com -> lebron.gma-demo100 // found! a feasible id
-    //
-    // c. in any cases(a or b), if generated Id is longer than FieldValidator.COURSE_ID_MAX_LENGTH, shorten the part
-    //    before "@" of the intial input email, by continuously remove its last character
-
-    /**
-     * Generate a course ID for demo course, and if the generated id already exists, try another one.
-     *
-     * @param instructorEmail is the instructor email.
-     * @return generated course id
-     */
-    private String generateDemoCourseId(String instructorEmail) {
-        String proposedCourseId = generateNextDemoCourseId(instructorEmail, FieldValidator.COURSE_ID_MAX_LENGTH);
-        while (logic.getCourse(proposedCourseId) != null) {
-            proposedCourseId = generateNextDemoCourseId(proposedCourseId, FieldValidator.COURSE_ID_MAX_LENGTH);
-        }
-        return proposedCourseId;
-    }
-
-    /**
-     * Generate a course ID for demo course from a given email.
-     *
-     * @param instructorEmail is the instructor email.
-     * @return the first proposed course id. eg.lebron@gmail.com -> lebron.gma-demo
-     */
-    private String getDemoCourseIdRoot(String instructorEmail) {
-        String[] emailSplit = instructorEmail.split("@");
-
-        String username = emailSplit[0];
-        String host = emailSplit[1];
-
-        String head = StringHelper.replaceIllegalChars(username, FieldValidator.REGEX_COURSE_ID, '_');
-        String hostAbbreviation = host.substring(0, 3);
-
-        return head + "." + hostAbbreviation + "-demo";
-    }
-
-    /**
-     * Generate a course ID for demo course from a given email or a generated course Id.
-     *
-     * <p>Here we check the input string is an email or course Id and handle them accordingly;
-     * check the resulting course id, and if bigger than maximumIdLength, cut it so that it equals maximumIdLength.
-     *
-     * @param instructorEmailOrProposedCourseId is the instructor email or a proposed course id that already exists.
-     * @param maximumIdLength is the maximum resulting id length allowed, above which we will cut the part before "@"
-     * @return the proposed course id, e.g.:
-     *         <ul>
-     *         <li>lebron@gmail.com -> lebron.gma-demo</li>
-     *         <li>lebron.gma-demo -> lebron.gma-demo0</li>
-     *         <li>lebron.gma-demo0 -> lebron.gma-demo1</li>
-     *         <li>012345678901234567890123456789.gma-demo9 -> 01234567890123456789012345678.gma-demo10 (being cut)</li>
-     *         </ul>
-     */
-    private String generateNextDemoCourseId(String instructorEmailOrProposedCourseId, int maximumIdLength) {
-        final boolean isFirstCourseId = instructorEmailOrProposedCourseId.contains("@");
-        if (isFirstCourseId) {
-            return StringHelper.truncateHead(getDemoCourseIdRoot(instructorEmailOrProposedCourseId),
-                                             maximumIdLength);
-        }
-
-        final boolean isFirstTimeDuplicate = instructorEmailOrProposedCourseId.endsWith("-demo");
-        if (isFirstTimeDuplicate) {
-            return StringHelper.truncateHead(instructorEmailOrProposedCourseId + "0",
-                                             maximumIdLength);
-        }
-
-        final int lastIndexOfDemo = instructorEmailOrProposedCourseId.lastIndexOf("-demo");
-        final String root = instructorEmailOrProposedCourseId.substring(0, lastIndexOfDemo);
-        final int previousDedupSuffix = Integer.parseInt(instructorEmailOrProposedCourseId.substring(lastIndexOfDemo + 5));
-
-        return StringHelper.truncateHead(root + "-demo" + (previousDedupSuffix + 1), maximumIdLength);
     }
 }
